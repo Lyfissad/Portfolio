349ddcbedeff3ac02fefc37613145a28f3461a1f
Muazzin <muazzin2009@gmail.com>
2025-04-20 02:38:34 +0500

    first complete build both mobile and desktop

--- src/component/ProjTile.jsx
+++ src/component/ProjTile.jsx
@@ -5,16 +5,19 @@ import { FaGithub } from "react-icons/fa";
 
 export default function ProjTile(props){
     return(
-        <div className="animate-fadeInL mt-7 bg-lightNavy w-[92%] h-96 md:w-{10%} shadow-2xl rounded-lg">
-            <div className="ml-5 pt-6">
-                <h3 className= "text-lg text-green font-geist animate-fadeInM "> Featured Project</h3>
-                <h2 className=" text-green mt-5 font-semiHead text-2xl">{props.projName}</h2>
+        <div className="animate-fadeInL mt-7 bg-lightNavy w-[92%] h-[415px] md:h-96 shadow-2xl rounded-lg">
+            <div className="ml-5 relative pt-6">
+                <h3 className= "text-lg text-green font-geist "> Featured Project</h3>
+                <h2 className=" text-slightFade md:hover:text-green md:cursor-pointer mt-5 font-semiHead text-xl md:text-2xl">{props.projName}</h2>
                 <h4 className="text-slate mt-8 text-left pr-5 text-md
-                 font-semiHead w-72 h-48 animate-fadeInM">{props.details}</h4>
-                 <h4 className="text-green right-11     font-geist text-md fixed">{props.stacks}</h4>
+                 font-semiHead w-5/6 md:w-3/4 md:text-md h-48">{props.details}</h4>
+                <a href="https://github.com/Lyfissad" className="no-underline size-7"><FaGithub className="fill-slightFade
+                   md:ml-6 absolute -bottom-12 md:relative md:bottom-0 md:hover:animate-pulse md:hover:fill-green md:hover:-translate-y-1
+                  transition-transform duration-250 size-7"/></a>
+                <h4 className="text-slate md:hover:text-green md:cursor-pointer
+                 absolute whitespace-break-spaces -bottom-12 md:bottom-0 right-4 font-semibold text-md md:text-lg">{props.stacks}</h4>
             </div>
-            <a href="https://github.com/Lyfissad" className="no-underline"><FaGithub className="fill-slightFade
-             ml-6 hover:animate-pulse size-7"/></a>    
+                
         </div>
     )
 }
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -5,12 +5,14 @@ import projects from "../project.json"
 export default function Projects(props){
     const proJObj = projects[0].projects
     return(
-        <div id="projects" className={`pl-9 mt-[30vh] md:mt-[60vh] mb-4 ${props.blur? "blur-sm shadow-lg z-2" :  ""}`}>
+        <div id="projects" className={`pl-9 md:relative md:left-[15%] mt-[30vh] md:mt-[60vh] mb-4 ${props.blur? "blur-sm shadow-lg z-2" :  ""}`}>
             <h1 className="text-2xl
-        text-green font-semiHead  animate-fadeInM">Some of my work<hr className="w-3/4 border-slightFade
-        border-y"/></h1>
-        <ProjTile projName = {proJObj.DMS.title} details = {proJObj.DMS.det} stacks = {proJObj.DMS.sNt}/>
-        <ProjTile projName = {proJObj.Hospital.title} details = {proJObj.Hospital.det} stacks= {proJObj.Hospital.sNt}/>
+            text-green font-semiHead md:text-3xl animate-fadeInM">Some of my work<hr className="w-10/12 md:w-3/5 relative top-4 border-darkSlate
+            border-y"/></h1>
+            <div className="md:mx-auto absolute md:flex gap-x-6 w-11/12 md:w-4/6 mt-5">
+                <ProjTile projName = {proJObj.DMS.title} details = {proJObj.DMS.det} stacks = {proJObj.DMS.sNt}/>
+                <ProjTile projName = {proJObj.Hospital.title} details = {proJObj.Hospital.det} stacks= {proJObj.Hospital.sNt}/>
+            </div>
         </div>
     )
 }
