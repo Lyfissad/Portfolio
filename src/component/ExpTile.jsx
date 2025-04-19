0199edb03304196a0f84c932ef1a999e4d9897be
Muazzin <muazzin2009@gmail.com>
2025-04-19 05:22:05 +0500

    experience done both mobile and pc moving to projects

--- src/component/ExpTile.jsx
+++ src/component/ExpTile.jsx
@@ -5,10 +5,10 @@ import { BiSolidRightArrow } from "react-icons/bi";
 
 export default function ExpiTile(props){
     return(
-        <div className="mt-7 md:relative md:left-[37%] midlg:left-[25rem] lg:left-[25rem]">
-            <h1 className="text-faded text-xl whitespace-break-spaces md:relative md:left-[3rem]">{props.position}<span className="text-green text-lg"> @ {props.company}</span></h1>
-            <h3 className="text-slightFade mt-4 md:relative md:left-[3rem]">{props.timeLine}</h3>
-            <ul className="text-slightFade mt-8 space-y-4 md:max-w-[40%] midlg:max-w-[%] md:ml-[3rem]">
+        <div className="md:relative">
+            <h1 className="text-faded text-xl whitespace-break-spaces md:relative ">{props.position}<span className="text-green text-lg"> @ {props.company}</span></h1>
+            <h3 className="text-slate mt-4 md:relative">{props.timeLine}</h3>
+            <ul className="text-slate mt-8 space-y-4 justify-start m-5 items-start md:max-w-[55%]">
                 <li className="flex text-base text-left mr-3"><BiSolidRightArrow className="fill-green pr-2 size-[25px] mt-1"/>{props.details[1]}</li>
                 <li className="flex text-base text-left mr-3"><BiSolidRightArrow className="fill-green pr-2 size-[25px] mt-1"/>{props.details[2]}</li>
                 <li className="flex text-base text-left mr-3"><BiSolidRightArrow className="fill-green pr-2 size-[25px] mt-1"/>{props.details[3]}</li>
--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -7,7 +7,7 @@ export default function Experience(props){
     const [activeTab, setActiveTab] = useState('ss')
     const activeTabStyle = "text-green transition-all duration-200 ease-in-out xs:-translate-y-3 md:-translate-x-4 md:flex md:translate-y-0"
     const activeTabHR = "transition-all ease-in-out duration-500 scale-x-75 md:h-6 md:w-px md:duration-200 origin-center md:ml-3 md:mt-1 border-green border-y md:border-x"
-    const baseTabStyle = "bg-navy cursor-pointer whitespace-nowrap h-10 text-lg md:text-xl md:px-16 md:py-2 md:duration-200 text-base font-semiHead"
+    const baseTabStyle =  "bg-navy cursor-pointer whitespace-nowrap h-12 w-40 md:w-5/6 text-lg md:hover:bg-lightNavy my-1 py-8 md:px-4 md:py-2 md:duration-200 font-semiHead md:flex md:items-center md:justify-center md:text-center rounded-md"
     const location = useLocation()
 
 
@@ -20,18 +20,18 @@ export default function Experience(props){
     <div id="exp" className={`ml-8 mb-4 relative top-8 md:top-40 md:left-[15%] midlg:ml-2 mt-[30vh] ${props.blur? "blur-sm shadow-lg z-2" :  ""}`}>
 
         <h2 className="text-2xl
-        text-green font-semiHead mt-6 mb-8 md:relative md:text-4xl animate-fadeInM">Where I've Worked
+        text-green font-semiHead mt-6 mb-8 md:relative md:text-3xl animate-fadeInM">Where I've Worked
         <hr className="w-10/12 md:w-3/5 relative top-4 md:top-6  border-darkSlate
         border-y"/></h2>
 
-        <div className="block md:flex">
-            <div className="flex  text-slightFade pl-4 md:w-72 scrollbar-thin gap-12 md:gap-1 overflow-x-scroll md:overflow-auto
-                md:flex-col md:h-80 md:relative md:left-[15rem] h-20 pt-4">
+        <div className="block md:flex md:relative top-8 left-[6%] gap-x-16 md:gap-x-[5px]">
+            <div className="flex  text-slightFade md:w-[25%] scrollbar-thin gap-12 md:gap-0 overflow-x-scroll md:overflow-visible
+                md:flex-col md:h-80 md:relative h-20">
                 <NavLink to={"/"} 
                         onClick={() => FlipState("ss")} 
                         className={
                             `${baseTabStyle} ${activeTab==="ss"?
-                            `${activeTabStyle}` : "text-faded md:hover:text-green"}`}>
+                            `${activeTabStyle}` : "text-slate md:hover:text-green"}`}>
                                 Synares Systems
                                 <hr className={`${activeTab==="ss"? `${activeTabHR}` : "scale-x-0"}`}/>
                 </NavLink>
@@ -40,7 +40,7 @@ export default function Experience(props){
                         onClick={() => FlipState("soft")} 
                         className={
                             `${baseTabStyle} ${activeTab==="soft"?
-                                `${activeTabStyle}`: "text-faded md:hover:text-green"}`}>
+                                `${activeTabStyle}`: "text-slate md:hover:text-green"}`}>
                                     Softpers
                                     <hr className={`${activeTab==="soft"? `${activeTabHR}` : "scale-x-0"}`}/>                   
                 </NavLink>
@@ -49,14 +49,14 @@ export default function Experience(props){
                 onClick={() => FlipState("vod")} 
                 className={
                     `${baseTabStyle} ${activeTab==="vod"? 
-                    `${activeTabStyle}`: "text-faded md:hover:text-green"}`}>
+                    `${activeTabStyle}`: "text-slate md:hover:text-green"}`}>
                         Vodworks
                         <hr className={`${activeTab==="vod"?  `${activeTabHR}`: "scale-x-0"}`}/>
                 </NavLink>
 
-                <NavLink to={"/err"} className="bg-navy hover:bg-lightNavy focus:text-green">Error</NavLink>
+                <NavLink to={"/err"} className="bg-navy hover:bg-lightNavy md:hidden focus:text-green">Error</NavLink>
             </div>
-                <div key={location.pathname} className="animate-fadeInNoMove">
+                <div key={location.pathname} className="animate-fadeInNoMove md:relative mt-8 md:top-2">
                     <Outlet />
                 </div>
             </div>
